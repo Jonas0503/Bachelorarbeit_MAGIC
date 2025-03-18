43e6176781ec029c22eb1d3f0f287b094df64686
Jonas Gilbert <jonas.gilbert.0503@gmail.com>
2025-03-18 17:07:45 +0100

    evaluation verify: changing_threshold

--- microcontroller/cw521/main_cw521.c
+++ microcontroller/cw521/main_cw521.c
@@ -316,9 +316,71 @@ void verify_changing_blocks(int start, int end, int interval, int number_of_meas
 
       results[f] = number_of_cycles;
 
-      volatile bool error = true;
-      if (res.correction_successful && (!is_bignum_not_zero(res.tag))) {
-        error = false;
+      volatile bool error_corrected = false;
+      if (res.correction_successful) {
+        error_corrected = true;
+      }
+
+      volatile int PRINT_VALUES = 42;
+    }
+
+    volatile float avg = average(results, number_of_measurements);
+    volatile float sd = standard_deviation(results, number_of_measurements, avg);
+
+    volatile int PRINT_VALUES = avg;
+  }
+}
+
+
+// to get an uncorrectable error at least two blocks are required and parameter error_in_ciphertext must be true
+void verify_changing_threshold(int start, int end, int interval, int number_of_measurements, int number_of_blocks, bool error_in_ciphertext, bool error_in_tag, bool uncorrectable_error) {
+  uint32_t key[8] = {
+    0xEAEBECED, 0xEEEFF0F1, 0xF2F3F4F5, 0xF6F7F8F9,
+    0xFAFBFCFD, 0xFEFF0001, 0x02030405, 0x06070809
+  };
+  uint32_t nonce[2] = {0x0, 0x0};
+
+  uint32_t one_block[4] = {0x12345678, 0xabcdef90, 0x87654321, 0x09fedcba};
+  uint32_t hash_hex[4] = {0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff};
+
+  for (int i = start; i <= end; i += interval) {
+    volatile uint32_t results[number_of_measurements];
+    for (int f = 0; f < number_of_measurements; f++) {
+      bignum blocks[number_of_blocks];
+      for (int k = 0; k < number_of_blocks; k++) {
+        blocks[k] = init_bignum(one_block);
+      }
+
+      bignum authorized_data = init_bignum_to_zero();
+      bignum hash_key = init_bignum(hash_hex);
+      bignum tag = ciphertext_blocks_to_tag(blocks, number_of_blocks, hash_key, authorized_data, key, nonce);
+
+      for (int k = 0; k < i; k++) {
+        if (error_in_ciphertext) {
+          blocks[0] = one_bit_modification(blocks[0], k);
+        }
+        if (error_in_tag) {
+          tag = one_bit_modification(tag, k);
+        }
+        // at least two blocks are required and parameter error_in_ciphertext must be true to get an uncorrectable error
+        if (uncorrectable_error) {
+          blocks[1] = one_bit_modification(blocks[1], k);
+        }
+      }
+
+      reset_timer();
+      start_timer();
+
+      volatile verify_result res = verify(authorized_data, blocks, number_of_blocks, tag, i, hash_key, key, nonce);
+
+      volatile uint32_t number_of_cycles = get_cycles();
+      stop_timer();
+
+      results[f] = number_of_cycles;
+
+      volatile bool error_corrected = false;
+      if (res.correction_successful) {
+        error_corrected = true;
       }
 
       volatile int PRINT_VALUES = 42;
@@ -469,7 +531,7 @@ int main(void)
 
   // ------------------------------------- my code start ---------------------------------------------------------------------
 
-  verify_changing_blocks(50, 250, 25, 5, 1, true, false, true);
+  verify_changing_blocks(1, 10, 1, 10, 1, true, false, true);
 
   // --------------------------------------- my code end -------------------------------------------------------------------------------------------
 
